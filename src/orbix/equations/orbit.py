fe82fcd2028a48b76717944cc0425bac9aa2eb45
Corey Spohn <cas584@cornell.edu>
2025-04-24 12:36:52 -0400

    chore: Update orbit functions

--- src/orbix/equations/orbit.py
+++ src/orbix/equations/orbit.py
@@ -2,19 +2,21 @@
 
 import jax.numpy as jnp
 
-from orbix.constants import G, twopi
+from orbix.constants import G, two_pi
+
+two_pi_G = two_pi * G
 
 
 def period_a(a, mu):
     """Orbital period from semi-major axis and standard gravitational parameter.
 
     Args:
-        a: float
+        a: Array
             Semi-major axis
-        mu: float
+        mu: Array
             Standard gravitational parameter
     Returns:
-        T: float
+        T: Array
             Orbital period
     """
     return 2 * jnp.pi * jnp.sqrt(a**3 / mu)
@@ -24,11 +26,11 @@ def period_n(n):
     """Orbital period from mean motion.
 
     Args:
-        n: float
+        n: Array
             Mean motion
 
     Returns:
-        T: float
+        T: Array
             Orbital period
     """
     return 2 * jnp.pi / n
@@ -38,12 +40,12 @@ def mean_motion(a, mu):
     """Mean motion from semi-major axis and standard gravitational parameter.
 
     Args:
-        a: float
+        a: Array
             Semi-major axis
-        mu: float
+        mu: Array
             Standard gravitational parameter
     Returns:
-        n: float
+        n: Array
             Mean motion
     """
     return jnp.sqrt(mu / a**3)
@@ -53,24 +55,24 @@ def semi_amplitude(T, Ms, Mp, e, i):
     """Semi-amplitude of the radial velocity curve from base quantities.
 
     Args:
-        T: float
+        T: Array
             Orbital period
-        Ms: float
+        Ms: Array
             Mass of the star
-        Mp: float
+        Mp: Array
             Mass of the planet
-        e: float
+        e: Array
             Eccentricity
-        i: float
+        i: Array
             Inclination
     Returns:
-        K: float
+        K: Array
             Semi-amplitude of the radial velocity curve
     """
     return (
-        (twopi * G / T) ** (1 / 3.0)
-        * (Mp * jnp.sin(i) / Ms ** (2 / 3.0))
-        * (1 - e**2) ** (-1 / 2)
+        (two_pi_G / T) ** (1 / 3.0)
+        * (Mp * jnp.sin(i) / (Ms ** (2 / 3.0)))
+        / jnp.sqrt(1 - e**2)
     )
 
 
@@ -78,46 +80,63 @@ def semi_amplitude_reduced(T, Ms, minimum_mass, sqrt_one_minus_e2):
     """Semi-amplitude of the radial velocity curve from pre-calculated quantities.
 
     Args:
-        T: float
+        T: Array
             Orbital period
-        Ms: float
+        Ms: Array
             Mass of the star
-        minimum_mass: float
+        minimum_mass: Array
             Mass of the planet multiplied by sin(i)
-        sqrt_one_minus_e2: float
+        sqrt_one_minus_e2: Array
             Square root of (1 - eccentricity^2)
 
     Returns:
-        K: float
+        K: Array
             Semi-amplitude of the radial velocity curve
     """
     return (
-        (twopi * G / T) ** (1 / 3.0)
-        * (minimum_mass / Ms ** (2 / 3.0))
+        (two_pi_G / T) ** (1 / 3.0)
+        * (minimum_mass / (Ms ** (2 / 3.0)))
         / sqrt_one_minus_e2
     )
 
 
-def mean_anomaly(t, n, M0, t0):
-    """Mean anomaly at time t (can be vector).
+def mean_anomaly_t0(t, n, M0, t0):
+    """Mean anomaly at time t (can be vector) from epoch.
 
     Requires that all units are consistent and does NOT clip the mean anomaly
     to the range [0, 2pi).
 
     Args:
-        t: float
+        t: Array
             Time
-        n: float
+        n: Array
             Mean motion
-        M0: float
+        M0: Array
             Mean anomaly at epoch
-        t0: float
+        t0: Array
             Epoch
     Returns:
-        M: float
+        M: Array
             Mean anomaly at time t
     """
     return n * (t - t0) + M0
+
+
+def mean_anomaly_tp(t, n, tp):
+    """Mean anomaly at time t (can be vector) from periapsis passage.
+
+    Args:
+        t: Array
+            Time
+        n: Array
+            Mean motion
+        tp: Array
+            Time of periapsis passage
+    Returns:
+        M: Array
+            Mean anomaly at time t
+    """
+    return n * (t - tp)
 
 
 def AB_matrices(a, e, i, W, w):
@@ -129,15 +148,15 @@ def AB_matrices(a, e, i, W, w):
     and I wouldn't dare disagree with him on this.
 
     Args:
-        a: float
+        a: Array
             Semi-major axis
-        e: float
+        e: Array
             Eccentricity
-        i: float
+        i: Array
             Inclination
-        W: float
+        W: Array
             Longitude of the ascending node
-        w: float
+        w: Array
             Argument of periapsis
     Returns:
         A: jnp.ndarray
@@ -150,18 +169,18 @@ def AB_matrices(a, e, i, W, w):
     sinW, cosW = jnp.sin(W), jnp.cos(W)
     sinw, cosw = jnp.sin(w), jnp.cos(w)
 
-    return AB_matrices_reduced(a, e, sini, cosi, sinW, cosW, sinw, cosw)
+    sqrt_one_minus_e2 = jnp.sqrt(1 - e**2)
+    return AB_matrices_reduced(a, sqrt_one_minus_e2, sini, cosi, sinW, cosW, sinw, cosw)
 
 
-def AB_matrices_reduced(
-    a, sqrt_one_minus_e, sini, sinW, cosW, sinw, cosw, sinwcosi, coswcosi
-):
+def AB_matrices_reduced(a, sqrt_one_minus_e2, sini, cosi, sinW, cosW, sinw, cosw):
     """Compute the A and B matrices from the trig values of the orbital elements.
 
     Args:
         a: Semi-major axis
-        sqrt_one_minus_e: Square root of (1 - eccentricity^2)
+        sqrt_one_minus_e2: Square root of (1 - eccentricity^2)
         sini: Sine of the inclination
+        cosi: Cosine of the inclination
         sinW: Sine of the longitude of the ascending node
         cosW: Cosine of the longitude of the ascending node
         sinw: Sine of the argument of periapsis
@@ -174,22 +193,24 @@ def AB_matrices_reduced(
         B: jnp.ndarray
             B matrix
     """
+    sinwcosi = sinw * cosi
+    coswcosi = cosw * cosi
     # Compute the A and B matrices as 3x1 JAX arrays
     A = a * jnp.array(
         [
-            [cosW * cosw - sinW * sinwcosi],
-            [sinW * cosw + cosW * sinwcosi],
-            [sinw * sini],
+            cosW * cosw - sinW * sinwcosi,
+            sinW * cosw + cosW * sinwcosi,
+            sinw * sini,
         ]
     )
     B = (
         a
-        * sqrt_one_minus_e
+        * sqrt_one_minus_e2
         * jnp.array(
             [
-                [-cosW * sinw - sinW * coswcosi],
-                [-sinW * sinw + cosW * coswcosi],
-                [cosw * sini],
+                -cosW * sinw - sinW * coswcosi,
+                -sinW * sinw + cosW * coswcosi,
+                cosw * sini,
             ]
         )
     )
